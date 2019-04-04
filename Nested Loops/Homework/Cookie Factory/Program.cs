8a55f5ba95cc4b1f331a30f9ab7fad7a3f35b2bf
Alexander <drumev00@gmail.com>
2019-04-04 14:46:11 +0300

    Added projects

---
+++ Nested Loops/Homework/Cookie Factory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookie_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = string.Empty;
+            int n = int.Parse(Console.ReadLine());
+            bool eggs = false;
+            bool flour = false;
+            bool sugar = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                product = Console.ReadLine();
+
+                while (product != "Bake!")
+                {
+                    switch (product)
+                    {
+                        case "eggs": eggs = true; break;
+                        case "flour": flour = true; break;
+                        case "sugar": sugar = true; break;
+                    }
+                    product = Console.ReadLine();
+                }
+                if (eggs && flour && sugar)
+                {
+                    Console.WriteLine($"Baking batch number {i}...");
+                     eggs = false;
+                     flour = false;
+                     sugar = false;
+                }
+                else
+                {
+                    Console.WriteLine("The batter should contain flour, eggs and sugar!");
+                    i--;
+                }
+            }
+        }
+    }
+}
---
+++ Nested Loops/Homework/Travelling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = string.Empty;
+            double savedMoney = 0;
+            double totalSavedMoney = 0;
+
+            while (true)
+            {
+                destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+                double requiredMoney = double.Parse(Console.ReadLine());
+
+                while (totalSavedMoney <= requiredMoney)
+                {
+                    savedMoney = double.Parse(Console.ReadLine());
+                    totalSavedMoney += savedMoney;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                totalSavedMoney = 0;
+            }
+        }
+    }
+}
