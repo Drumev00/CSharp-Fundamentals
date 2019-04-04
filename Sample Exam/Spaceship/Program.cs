becc7cc6ea447a6c7f840a0cf5cf2ce765b08349
Alexander <drumev00@gmail.com>
2019-04-04 15:11:32 +0300

    Added Sample exam

---
+++ Sample Exam/Spaceship/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double shipWidth = double.Parse(Console.ReadLine());
+            double shipLength = double.Parse(Console.ReadLine());
+            double shipHeigth = double.Parse(Console.ReadLine());
+            double averageAstronautHeigth = double.Parse(Console.ReadLine());
+
+            double shipValue = shipWidth * shipLength * shipHeigth;
+            double roomValue = (averageAstronautHeigth + 0.40) * 2 * 2;
+
+            double result = Math.Floor(shipValue / roomValue);
+            
+            if (result >= 3 && result <= 10)
+            {
+                Console.WriteLine($"The spacecraft holds {result} astronauts.");
+            }
+            else if (result < 3)
+            {
+                Console.WriteLine("The spacecraft is too small.");
+            }
+            else if (result > 10)
+            {
+                Console.WriteLine("The spacecraft is too big.");
+            }
+        }
+    }
+}
