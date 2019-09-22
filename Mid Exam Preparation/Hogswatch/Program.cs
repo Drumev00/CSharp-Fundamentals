56965d2f77779b7c75834647b2c9ce0800de80d2
Alexander <drumev00@gmail.com>
2019-09-22 21:49:08 +0300

    Solved second exercise

---
+++ Mid Exam Preparation/Hogswatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Hogswatch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int initialPresents = int.Parse(Console.ReadLine());
+			int startingPresents = initialPresents;
+			int count = 0;
+			bool heWentBack = false;
+			int additional = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+				int amountOfSocks = int.Parse(Console.ReadLine());
+				initialPresents -= amountOfSocks;
+				if (initialPresents < 0)
+				{
+					heWentBack = true;
+					count++;
+					int neededPresets = Math.Abs(initialPresents);
+					int remainedHomes = n - i;
+					initialPresents = (startingPresents / i) * remainedHomes + neededPresets;
+					additional += initialPresents;
+					initialPresents -= neededPresets;
+				}
+			}
+			if (heWentBack)
+			{
+				Console.WriteLine($"{count}");
+				Console.WriteLine($"{additional}");
+			}
+			else
+			{
+				Console.WriteLine(initialPresents);
+			}
+			
+		}
+	}
+}
