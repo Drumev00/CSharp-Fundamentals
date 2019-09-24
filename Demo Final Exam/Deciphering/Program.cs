a359c4381b8dfa4f4e6be259f4f4d498463f5591
Alexander <drumev00@gmail.com>
2019-09-24 17:31:14 +0300

    Problem's solution

---
+++ Demo Final Exam/Deciphering/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Deciphering
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string script = Console.ReadLine();
+			bool isValid = false;
+
+			for (int i = 0; i < script.Length; i++)
+			{
+				if (script[i] == '{' || script[i] == '}' || script[i] == '|' || script[i] == '#' || script[i] >= 100 && script[i] <= 122)
+				{
+					isValid = true;
+				}
+				else
+				{
+					Console.WriteLine("This is not the book you are looking for.");
+					isValid = false;
+					break;
+				}
+			}
+			if (isValid)
+			{
+				string decodedScript = string.Empty;
+				for (int i = 0; i < script.Length; i++)
+				{
+					decodedScript += (char)(script[i] - 3);
+				}
+				string stringToReplace = Console.ReadLine();
+				string[] tokens = stringToReplace.Split();
+				for (int i = 0; i < decodedScript.Length; i++)
+				{
+					if (decodedScript.Contains(tokens[0]))
+					{
+						decodedScript = decodedScript.Replace(tokens[0], tokens[1]);
+					}
+				}
+				Console.WriteLine(decodedScript);
+			}
+		}
+	}
+}
---
+++ Demo Final Exam/Dictionary/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dictionary = new Dictionary<string, List<string>>();
+			string input = Console.ReadLine();
+			string[] tokens = input.Split(" | ");
+
+			var words = new List<string>();
+			var descriptions = new List<string>();
+			int wordIndex = 0;
+			int descriptionIndex = 1;
+			for (int i = 0; i < tokens.Length; i++)
+			{
+				string[] wordsAndDescriptions = tokens[i].ToString().Split(": ");
+				words.Add(wordsAndDescriptions[wordIndex]);
+				descriptions.Add(wordsAndDescriptions[descriptionIndex]);
+				if (!dictionary.ContainsKey(words[i]))
+				{
+					dictionary[words[i]] = new List<string>();
+				}
+				dictionary[words[i]].Add(descriptions[i]);
+			}
+			input = Console.ReadLine();
+			bool listAsInput = false;
+			while (true)
+			{
+				if (input == "End" || input == "List")
+				{
+					if (input == "List")
+					{
+						listAsInput = true;
+					}
+					break;
+				}
+				tokens = input.Split(" | ");
+				for (int i = 0; i < tokens.Length; i++)
+				{
+					if (dictionary.ContainsKey(tokens[i]))
+					{
+						Console.WriteLine($"{tokens[i]}");
+						foreach (var kvp in dictionary[tokens[i]].OrderByDescending(x => x.Length))
+						{
+							Console.WriteLine($" -{kvp}");
+						}
+					}
+				}
+				input = Console.ReadLine();
+			}
+			if (listAsInput)
+			{
+				foreach (var word in dictionary.OrderBy(x => x.Key))
+				{
+					Console.Write($"{word.Key} ");
+				}
+			}
+		}
+	}
+}
