92995f88c2995336fe1e13a475575c7d3b39c4c5
Alexander <drumev00@gmail.com>
2019-09-25 22:23:58 +0300

    Exam exercise solved

---
+++ Final Exam/Arriving In Kathmandu/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Arriving_In_Kathmandu
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			
+
+			while (input != "Last note")
+			{
+				string peakPattern = @"^[A-Za-z!@#$?\d]+(?==)";
+				string lengthPattern = @"(?<=)\d+(?=<)";
+				string geohashPattern = @"(?<=<<)[\w\d\s\S\W\D]+";
+				string text = $"{peakPattern}={lengthPattern}<<{geohashPattern}";
+
+				bool peakIsValid = Regex.IsMatch(input, peakPattern);
+				bool lengthIsValid = Regex.IsMatch(input, lengthPattern);
+				bool geohashIsValid = Regex.IsMatch(input, geohashPattern);
+				Regex regex = new Regex(text);
+
+				if (!peakIsValid || !lengthIsValid || !geohashIsValid)
+				{
+					Console.WriteLine("Nothing found!");
+				}
+				else if (peakIsValid && lengthIsValid && geohashIsValid)
+				{
+					var peakPart = Regex.Match(input, peakPattern);
+					var lengthPart = Regex.Match(input, lengthPattern);
+					string number = lengthPart.ToString();
+					var geohashPart = Regex.Match(input, geohashPattern);
+					string geohash = geohashPart.ToString();
+					if (int.Parse(number) != geohash.Length)
+					{
+						Console.WriteLine("Nothing found!");
+					}
+					else
+					{
+						string stringToClear = peakPart.ToString();
+						foreach (var symbol in stringToClear)
+						{
+							if (symbol == '!' || symbol == '@' || symbol == '#' || symbol == '$' || symbol == '?')
+							{
+								int index = stringToClear.IndexOf(symbol);
+								stringToClear = stringToClear.Remove(index, 1);
+							}
+						}
+						Console.WriteLine($"Coordinates found! {stringToClear} -> {geohashPart}");
+					}
+				}
+				input = Console.ReadLine();
+			}
+			
+		}
+	}
+}
---
+++ Final Exam/On the Way To Annapurna/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace On_the_Way_To_Annapurna
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string[] tokens = input.Split(new string[] { "->", "," }, StringSplitOptions.RemoveEmptyEntries);
+			var storesAndProducts = new Dictionary<string, List<string>>();
+
+			while (input != "END")
+			{
+				string action = tokens[0];
+				string store = tokens[1];
+
+				if (action == "Add" && tokens.Length < 3)
+				{
+					if (!storesAndProducts.ContainsKey(store))
+					{
+						storesAndProducts[store] = new List<string>();
+					}
+					storesAndProducts[store].Add(tokens[3]);
+				}
+				else if (action == "Add" && tokens.Length >= 2)
+				{
+					if (!storesAndProducts.ContainsKey(store))
+					{
+						storesAndProducts[store] = new List<string>();
+					}
+					for (int i = 2; i < tokens.Length; i++)
+					{
+						storesAndProducts[store].Add(tokens[i]);
+					}
+				}
+				else if (action == "Remove")
+				{
+					if (storesAndProducts.ContainsKey(store))
+					{
+						storesAndProducts.Remove(store);
+					}
+				}
+
+				input = Console.ReadLine();
+				tokens = input.Split(new string[] { "->", "," }, StringSplitOptions.RemoveEmptyEntries);
+			}
+			Console.WriteLine("Stores list:");
+			foreach (var kvp in storesAndProducts.OrderByDescending(x => x.Value.Count).ThenByDescending(x => x.Key))
+			{
+				Console.WriteLine($"{kvp.Key}");
+				foreach (var item in kvp.Value)
+				{
+					Console.WriteLine($"<<{item}>>");
+				}
+			}
+		}
+	}
+}
